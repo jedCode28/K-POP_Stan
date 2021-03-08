9351f53c8508ca515c600a4a4ced4a9840f7b079
jamesElls <jamellsworth@gmail.com>
2021-03-08 16:57:45 -0700

    fixed edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Bands from './components/Bands';
+import EditBand from './components/EditBand';
+
 function App() {
   return (
     <div className="App">
       <h1>kpop</h1>
       
-
+    <EditBand />
     </div>
   );
 }
--- client/src/components/EditBand.js
+++ client/src/components/EditBand.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
+import axios from 'axios'
+const EditBand = (props) => {
 
-const EditBand = ({ id, name, description, sales}) => {
+    const {id} = props
     
+    const [name, setName] = useState(props.name)
+    const [description, setDescription] = useState(props.description)
+    const [sales, setSales] = useState(props.sales)
+
+
+
+
     const handleSubmit = async (e) => {
        e.preventDefault()
        let band = {name, description, sales}
        console.log(band)
        
-       let res = await axios.put(`/api/facts/${id}`, band)
+       let res = await axios.put(`/api/bands/${id}`, band)
        console.log(res)
     }
 
@@ -17,14 +26,15 @@ const EditBand = ({ id, name, description, sales}) => {
             <h1>Edit Band Details</h1>
             <form onSubmit={handleSubmit}>
                 <h2>Name</h2>
-                <input value={text} onChange={(e) => setText(e.target.value)} />
+                <input value={name} onChange={(e) => setName(e.target.value)} />
 
                 <h2>Description</h2>
-                <input value={description} onChange={(e) => setText(e.target.value)} />
+                <input value={description} onChange={(e) => setDescription(e.target.value)} />
 
                 <h2>Album Sales</h2>
-                <input value={sales} onChange={(e) => setText(e.target.value)} />
+                <input value={sales} onChange={(e) => setSales(e.target.value)} />
 
+                <button type={'submit'}>Update</button>
             </form>
             
         </div>
