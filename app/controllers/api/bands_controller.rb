f0c14dd2df6ef5b4e1bcef2ef9b1c89aa8f83689
Derek cluff <cluffderek@yahoo.com>
2021-03-08 15:33:27 -0700

    controller done back end mostly complete

--- app/controllers/api/bands_controller.rb
+++ app/controllers/api/bands_controller.rb
@@ -1,2 +1,34 @@
 class Api::BandsController < ApplicationController
+def index 
+  render json: Band.all
+end
+
+def create 
+  band = Band.new (band_params)
+  if band.save
+      render json: band
+  else 
+    render json: {errors: band.errors}, status: :unprocessable_entity 
+  end 
+end
+
+def update 
+  band = Band.find(params[:id])
+  if band.update(band_params)
+    render json: band
+  else 
+    render json: {errors: band.errors}, status: :unprocessable_entity 
+  end
+end
+
+def destroy 
+  band = Band.find(params[:id]).destroy
+  render json: band
+end
+
+private
+
+def band_params
+  params.require(:band).permit(:name, :description, :sales)
+  end 
 end
